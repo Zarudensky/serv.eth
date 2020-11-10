d7d9f97ace1eddb6320976a35552aef58579dbf6
Stas Zarudensky <szarudensky@gmail.com>
2020-11-11 00:32:20 +0200

    Add header

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('load main js');
+const btnOpenMenu = document.querySelector('#btnOpenMenu');
+const btnCloseMenu = document.querySelector('#btnCloseMenu');
+const headerContainer = document.querySelector('.header__container');
+
+
+btnOpenMenu.addEventListener('click', onBtnOpenMenu);
+btnCloseMenu.addEventListener('click', onBtnCloseMenu);
+headerContainer.addEventListener('click', onBtnCloseMenu);
+
+
+function onBtnOpenMenu(e) {
+  e.preventDefault();
+  openMenu();
+}
+
+function openMenu() {
+  headerContainer.classList.add('open');
+}
+
+function onBtnCloseMenu(e) {
+  e.preventDefault();
+  closeMenu();
+}
+
+function closeMenu() {
+  headerContainer.classList.remove('open');
+}
